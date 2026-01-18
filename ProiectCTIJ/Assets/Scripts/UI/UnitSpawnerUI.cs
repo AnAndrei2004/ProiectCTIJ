904df280debdcb21d5d82764d007ad010e01fa6e
BengulescuRobert <sebirobiantonio@gmail.com>
2026-01-18 21:03:12 +0200

    Add player spawn system via Head UI portraits

---
+++ ProiectCTIJ/Assets/Scripts/UI/UnitSpawnerUI.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UnitSpawnerUI : MonoBehaviour
+{
+    [Header("Unit Buttons")]
+    public Button[] unitButtons = new Button[3];
+    public Image[] unitIcons = new Image[3];
+    public TextMeshProUGUI[] unitCostTexts = new TextMeshProUGUI[3];
+    public TextMeshProUGUI[] unitNameTexts = new TextMeshProUGUI[3];
+    
+    [Header("Cooldown Display")]
+    public TextMeshProUGUI spawnCooldownText;
+    
+    private float lastSpawnTime = -999f;
+
+    private void Start()
+    {
+        // Conectează butoanele cu funcții de spawn
+        for (int i = 0; i < unitButtons.Length; i++)
+        {
+            int index = i; // Closure variable
+            if (unitButtons[i] != null)
+            {
+                unitButtons[i].onClick.AddListener(() => OnUnitButtonClicked(index));
+            }
+            
+            // Actualizează text-urile cu informații despre unitate
+            if (unitNameTexts[i] != null && UnitSpawner.Instance != null)
+            {
+                unitNameTexts[i].text = UnitSpawner.Instance.GetPlayerUnitName(i);
+            }
+            if (unitCostTexts[i] != null && UnitSpawner.Instance != null)
+            {
+                unitCostTexts[i].text = $"${UnitSpawner.Instance.GetPlayerUnitCost(i)}";
+            }
+        }
+    }
+
+    private void Update()
+    {
+        // Afișează cooldown-ul
+        if (spawnCooldownText != null && UnitSpawner.Instance != null)
+        {
+            float timeSinceLastSpawn = Time.time - lastSpawnTime;
+            if (timeSinceLastSpawn < UnitSpawner.Instance.spawnCooldown)
+            {
+                float remainingTime = UnitSpawner.Instance.spawnCooldown - timeSinceLastSpawn;
+                spawnCooldownText.text = $"Cooldown: {remainingTime:F1}s";
+                spawnCooldownText.gameObject.SetActive(true);
+                
+                // Dezactivează butoanele în cooldown
+                foreach (Button btn in unitButtons)
+                {
+                    if (btn != null) btn.interactable = false;
+                }
+            }
+            else
+            {
+                spawnCooldownText.gameObject.SetActive(false);
+                
+                // Reactivează butoanele
+                foreach (Button btn in unitButtons)
+                {
+                    if (btn != null) btn.interactable = true;
+                }
+            }
+        }
+    }
+
+    private void OnUnitButtonClicked(int unitIndex)
+    {
+        if (UnitSpawner.Instance != null)
+        {
+            UnitSpawner.Instance.SpawnPlayerUnit(unitIndex);
+            lastSpawnTime = Time.time;
+        }
+    }
+
+    public void SetUnitIcon(int index, Sprite sprite)
+    {
+        if (index >= 0 && index < unitIcons.Length && unitIcons[index] != null)
+        {
+            unitIcons[index].sprite = sprite;
+        }
+    }
+}
